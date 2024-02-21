5c99846b10f4457db5f2d8504d6f1d40e1e8f671
Sir-Thom <Sir-Thom1702@pm.me>
2024-02-20 21:02:47 -0500

    started drag

--- src/components/Todo.ts
+++ src/components/Todo.ts
@@ -1,34 +1,41 @@
-import { GridStack } from 'gridstack';
 import * as ko from 'knockout';
-//let grid = GridStack.init({ cellHeight:  70 , draggable: { handle: '.drag-handle' }, staticGrid: false });
+import 'gridstack/dist/gridstack.min.css';
+import { GridStack } from 'gridstack';
 
-function taskItem(this: any, task, isDone) {
-  this.task = ko.observable(task);
-  this.isDone = ko.observable(isDone);
+function taskItem(task: string, isDone: boolean) {
+    this.task = ko.observable(task);
+    this.isDone = ko.observable(isDone);
 }
 
 function TodoViewModel(this: any) {
-    
-    var self = this;
+    const self = this;
     self.taskInput = ko.observable("");
     self.taskItems = ko.observableArray([]);
     
     self.addTask = function() {
         self.taskItems.push(new taskItem(self.taskInput(), false));
         self.taskInput("");
-      
-
     };
-
 
     self.removeTask = function(task: any) {
-        
         self.taskItems.remove(task);
     };
+
+    // Initialize Gridstack
+    const grid = GridStack.init({
+        cellHeight: 'auto',
+        
+        float: true,
+        animate: true,
+        disableOneColumnMode: true,
+        
+    });
+    grid.enableMove(true, '.grid-stack-item');
+    grid.enableResize(false, '.grid-stack-item');
+    grid.setStatic(false);
+
    
 
-    //grid.load(self.taskItems().slice());
 }
 
 ko.applyBindings(new TodoViewModel());
-
